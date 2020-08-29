ac7ffc3e1147fc2f350a31bf9fe33911e3b0d0f6
narutopig <redgerxu@gmail.com>
2020-08-28 21:16:25 -0700

    moved cmds to index.js and deleted cmds.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const prefix = process.env.prefix;
 const https = require('https');
 
 client.on('ready', function(){
-    const cmds = require('./cmds.js');
     console.log('Logged in.')
     client.user.setActivity(`${prefix}help | v${pkg.version}`);
 });
@@ -29,24 +28,120 @@ client.on('message', function(message){
     var args = message.content.split(' ').slice(1);
     switch (command.toUpperCase()){
         case 'HELP':
-            cmds.help(message);
+            help(message);
             break;
         case 'HI':
-            cmds.hello(message);
+            hello(message);
             break;
         case 'PURGE':
-            cmds.purge(message,args);
+            purge(message,args);
             break;
         case 'STATS':
-            cmds.stats(message);
+            stats(message);
             break;
         case 'COVID':
-            cmds.covid(message);
+            covid(message);
             break;
         case 'BUG':
             bug(message,args);
     }
     console.log(`Command: ${command}\nArgs: ${args}`);
 })
+
+
+function formatNumber(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+function help(message){
+    message.channel.send('No help for you nerd');
+}
+function hello(message){
+    message.channel.send('Hello!');
+}
+function purge(message,args){
+    if (message.member.hasPermission('MANAGE_MESSAGES')){
+        try{
+            let amt = parseInt(args[0]);
+            if (amt < 1){
+                message.channel.send('Value must be at least 1');
+            }
+            let val = Math.min(100,amt);
+            let temp = amt;
+            while (temp > 0){
+                temp = temp - val;
+                message.channel.bulkDelete(val);
+                val = Math.min(100,amt);
+            }
+            console.log(val,temp);
+            message.channel.send('Success!');
+        }
+        catch(err){
+            console.log(err);
+            message.channel.send('Something went wrong');
+        }
+    }
+    else{
+        message.channel.send('You can\' do that!');
+    }
+}
+function stats(message){
+    let days = Math.floor(client.uptime / 86400000);
+    let hours = Math.floor(client.uptime / 3600000) % 24;
+    let minutes = Math.floor(client.uptime / 60000) % 60;
+    var uptime = `${days}d ${hours}h ${minutes}m`;
+    if (minutes == 0){
+        uptime = "Less than a minute"
+    }
+    var ping = client.ws.ping.toString() + " ms";
+    var memberCount = message.guild.memberCount;
+    const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Stats')
+        .setAuthor('Mineware Bot')
+        .addFields(
+            {name: 'Members', value: memberCount, inline: false},
+        )
+        .setTimestamp()
+        .setFooter(`Uptime: ${uptime}`, client.user.avatar_url);
+
+    message.channel.send(embed);
+}
+function covid(message){ // sends a discord.MessageEmbed
+    let url = `https://api.covidtracking.com/v1/us/current.json`;
+    https.get(url,(res) => {
+    let body = "";
+
+    res.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    res.on("end", () => {
+        try {
+            let json = JSON.parse(body);
+            let data = json[0];
+            const covidEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Covid-19 Stats')
+                .setAuthor('Mineware Bot')
+                .addFields(
+                    {name: "Positive:", value: formatNumber(data.positive), inline: false},
+                    {name: "Negative:", value: formatNumber(data.negative), inline: false}
+                )
+                .addFields(
+                    {name: "Deaths:", value: formatNumber(data.death), inline: false},
+                    {name: "Recovered", value: formatNumber(data.recovered),inline: false}
+                )
+                .setTimestamp()
+                .setFooter(`Data from ${url}`, client.user.avatar_url);
+            message.channel.send(covidEmbed);
+        }
+        catch (error) {
+            console.error(error.message);
+        };
+    });
+}).on("error", (error) => {
+    console.error(error.message);
+});
+}
 console.log(pkg);
 client.login(token);
