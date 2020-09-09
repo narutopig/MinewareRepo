6911e1ae5e7fe65d49cb2fd05fec409437674997
narutopig <redgerxu@gmail.com>
2020-09-09 13:39:25 -0700

    rr test

---
+++ commands/rr.js
@@ -0,0 +1,10 @@
+client.on('')
+module.exports = {
+    'name': 'rr',
+    'description': 'Change the reaction roles',
+    'arguments': 'Channel, role, message (list of words)',
+    'permissions': 'MANAGE_ROLES',
+    execute(message,args,client){
+        message.react(':smile:');
+    }
+}
