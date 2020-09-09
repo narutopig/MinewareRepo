dccec3bcef83c6491455337b7bd83023164be091
narutopig <redgerxu@gmail.com>
2020-09-09 16:16:37 -0700

    self role update

---
+++ commands/selfrole.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const srjson = fs.readFileSync('./resources/settings.json');
+let sr = JSON.parse(srjson);
+module.exports = {
+    'name': 'rr',
+    'description': 'Change the reaction roles',
+    'arguments': 'Channel, role, message (list of words)',
+    'permissions': 'MANAGE_ROLES',
+    execute(message,args,client){
+        if (!message.member.hasPermission('MANAGE_ROLES')){
+            message.channel.send(`You need the ${this.permissions} permission(s) to use this command!`);
+            return;
+        }
+        const channel = message.mentions.channels.first();
+        const role = message.mentions.roles.first();
+        const words = args.slice(2);
+        if (!words){
+            message.channel.send('Please provide a message.');
+            return;
+        }
+        console.log(sr);
+    }
+}
