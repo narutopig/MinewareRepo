b3d894019291c1206e36281fd9d3184885d523bc
MalfunctioningRobots <56807509+MalfunctioningRobots@users.noreply.github.com>
2020-09-09 11:17:45 -0700

    udpatead giveawaysad.js
    itejlsk jd not finisehdd yet ok

--- commands/giveaway.js
+++ commands/giveaway.js
@@ -1,1 +1,22 @@
+const Discord = require("discord.js")
 
+module.export = {
+    'name': 'announce',
+    'description': 'Make an announcement!',
+    'arguments': 'Words',
+    'permissions': 'MANAGE_CHANNELS',
+    execute(message,args,client){    // args[0]: time, args[1:]: item
+      if(!message.member.hasPermission('MANAGE_CHANNELS')){
+        message.channel.send('You need the \`MANAGE_CHANNELS`\ permission to use this command!');
+        return;
+      }
+      
+      function makeGiveaway(time, item){
+        cont giveawayEmbed = 
+      }
+      
+      time = parseInt(args[0])
+      
+      
+    }
+}
