f8496b50893b13170ac46c975a13c898890fc1a7
narutopig <redgerxu@gmail.com>
2020-09-09 10:37:51 -0700

    Added embed for kick, mute, and ban

--- commands/mute.js
+++ commands/mute.js
@@ -8,17 +8,49 @@ module.exports = {
             message.channel.send(`You need the ${this.permissions} permission(s) to use this command!`);
             return;
         }
-        const person = message.mentions.users.first();
+        let target = message.mentions.members.first();
+        if (!message.member.hasPermission('BAN_MEMBERS')){
+            message.channel.send(`You need the ${this.permissions} permission(s) to use this command.`);
+            return;
+        }
+        if (!target){
+            message.channel.send(`Please mention someone to ${this.name}`);
+            return;
+        }
+        if (target.user.id == message.author.id){
+            message.channel.send(`You can\'t ${this.name} yourself.`);
+            return;
+        }
+        if (!target.manageable){
+            message.channel.send(`I cannot ${this.name} that person`);
+            return;
+        }
+        if (target.hasPermission('ADMINISTRATOR')){
+            message.channel.send(`I cannot ${this.name} administrators`);
+            return;
+        }
         if (person.roles.cache.some(role => role.name.toUpperCase() == 'MUTED')){
             message.channel.send('That user is already muted (or has a role named Muted');
             return;
         }
-        if (person.roles.find(role => role.hasPermission('Administrator'))){
-            message.channel.send(`You cannot ${this.name} administrators`);
+        let role = message.guild.roles.find(r => r.name.toUpperCase() == "Muted");
+        if (!role){
+            message.channel.send('No mute role found, please create a role named \"Muted\"');
             return;
         }
-        let role = message.guild.roles.find(r => r.name.toUpperCase() == "Muted");
-        person.roles.add(role);
-        message.channel.send(`Muted ${person.toString()}`);
+        let reason = 'None';
+        if (args[1]){
+            reason = args[1];
+        }
+        target.roles.add(role);
+        const embed = new Discord.MessageEmbed()
+            .setTitle(`${this.name}`)
+            .setDescription(`Muted ${target} (${target.displayName})`)
+            .setColor("#ff0000")
+            .addFields(
+                {name: 'Reason', value: reason, inline: false}
+            )
+            .setFooter(`Muted by ${message.author.username}`);
+        message.channel.send(embed);
     }
 }
