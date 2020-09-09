84c712d316df3eab2dfcadc12a105cacd8e7815b
narutopig <redgerxu@gmail.com>
2020-09-09 14:25:03 -0700

    test rr

--- commands/rr.js
+++ commands/rr.js
@@ -4,6 +4,18 @@ module.exports = {
     'arguments': 'Channel, role, message (list of words)',
     'permissions': 'MANAGE_ROLES',
     execute(message,args,client){
-        message.react('😀');
+        if (!message.member.hasPermission('MANAGE_ROLES')){
+            message.channel.send(`You need the ${this.permissions} permission(s) to use this command!`);
+            return;
+        }
+        const channel = message.mentions.channels.first();
+        const role = message.mentions.roles.first();
+        const words = args.slice(2);
+        if (!words){
+            message.channel.send('Please provide a message.');
+            return;
+        }
+        channel.send(words.join(' '))
+            .then(message => console.log(message.id));
     }
 }
